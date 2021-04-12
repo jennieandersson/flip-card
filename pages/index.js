7fac4c9ca647e24ed0e61f1e5ba302d2caf681a6
Jennie <jennie.andersson@vinnovera.se>
2021-04-12 12:22:06 +0200

    Add animations

--- pages/index.js
+++ pages/index.js
@@ -1,155 +1,123 @@
 import Head from 'next/head'
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import cn from 'classnames'
+import { motion, useMotionValue, useTransform, AnimatePresence } from 'framer-motion'
 
 export default function Home() {
 
   const [isFlipped, setIsFlipped] = useState(false)
+  const [check, setCheck] = useState(false)
   const classes = cn({
     'card': true,
     'flipped': isFlipped
-  });
+  })
 
-  console.log(classes)
+  const alternatives = [
+    'Det spelar ingen roll hur jag ser ut, vilken färg min hud har, vilken religion jag tillhör, var jag kommer ifrån.',
+    'Vuxna som bestämmer över mig behöver inte respektera mig.',
+    'Man får göra mig illa eller vara elak mot mig för något som jag eller min familj är eller har sagt eller gjort.'
+  ]
+
+  const divVariants = {
+    show: { 
+      transition: {
+        staggerChildren: .4,
+        delayChildren: .8,
+      }
+    },
+    hide: { 
+      transition: {
+        staggerChildren: .1,
+        staggerDirection: -1,
+        delayChildren: .4
+      }
+     },
+     check: { 
+      transition: {
+        staggerChildren: .2,
+        staggerDirection: -1,
+      }
+     }
+  }
+
+  const buttonVariants = {
+    show: { 
+      y: 0, 
+      opacity: 1,
+      height: 'auto'
+    },
+    hide: { 
+      y: -20, 
+      opacity: 0, 
+      height: 0 
+    },
+    check: { 
+      y: 0, 
+      x: -40, 
+      opacity: 0,
+      height: 'auto'
+    }
+  }
 
   return (
     <div className="container">
-      <div className={classes} onClick={() => {setIsFlipped(!isFlipped)}}>
+      <motion.div 
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+        className={classes} 
+        onClick={() => {setIsFlipped(true)}}
+      >
         <div className="content">
           <div className="front-card"></div>
-          <div className="back-card">
-            <div className="statement">
-              <p>
-                <strong>Alla barn i hela världen har samma rättigheter</strong>
-              </p>
-            </div>
-            <button className="button">
-              <p>
-                Det spelar ingen roll hur jag ser ut, vilken färg min hud har,
-                vilken religion jag tillhör, var jag kommer ifrån.
-              </p>
-            </button>
-            <button className="button">
-              <p>Vuxna som bestämmer över mig behöver inte respektera mig.</p>
-            </button>
-            <button className="button">
-              <p>
-                Man får göra mig illa eller vara elak mot mig för något som jag
-                eller min familj är eller har sagt eller gjort.
-              </p>
-            </button>
-            <button className="button check">
-              <p>Rätta</p>
-            </button>
-          </div>
+          <AnimatePresence>
+            {isFlipped && (
+              <div className="back-card">
+                {check && (
+                  <motion.button
+                  initial={{ opacity: 0, scale: 0 }}
+                  animate={{ opacity: 1, scale: [.4, .7, 1.1, 1, 1.1, 1] }}
+                  transition={{ duration: 1, delay: .7 }} 
+                    className="button correct-answer" 
+                  >
+                    <p>
+                      {alternatives[0]}
+                    </p>
+                  </motion.button>
+                )}
+                <motion.div
+                  className="statement"
+                >
+                  <p>
+                    <strong>Alla barn i hela världen har samma rättigheter</strong>
+                  </p>
+                </motion.div>
+                <motion.div 
+                  variants={ divVariants }
+                  initial="hide" // ulVariants[]
+                  animate={check ? 'check' : 'show'}
+                  exit="hide"
+                >
+                  {alternatives.map((alternative, index) => (
+                    <motion.button 
+                      variants={buttonVariants} 
+                      className="button" 
+                      key={index}
+                    >
+                      <p>
+                        {alternative}
+                      </p>
+                    </motion.button>
+                  ))}
+                </motion.div>
+                <button className="button check" onClick={() => {setCheck(true)}}>
+                  <p>Rätta</p>
+                </button>
+              </div>
+            )}
+          </AnimatePresence>
         </div>
-      </div>
-      <style jsx>{`
-        * {
-          box-sizing: border-box;
-        }
-        
-        .container {
-          font-family: sans-serif;
-          text-align: center;
-          padding: 3em;
-
-        }
-        
-        .button {
-          width: 100%;
-          border: none;
-          background: #a6ddf1;
-          padding: 0.3em 0.7em;
-          border-radius: 0.5em;
-          margin-top: 1em;
-          box-shadow: 0 4px 1px -1px #6dc5e6;
-          cursor: pointer;
-          border: 3px solid transparent;
-        }
-        
-        .button:hover {
-          background: #bdecff;
-          border: 3px solid #6dc5e6;
-          box-shadow: none;
-        }
-        
-        .statement {
-          width: 100%;
-          padding: 1em 3em;
-          border-radius: 0.5em;
-          background: #d39cc7;
-          box-shadow: none;
-        }
-        
-        .check {
-          color: #fff;
-          background: #04a83e;
-          box-shadow: 0 4px 1px -1px #028931;
-          border: none;
-        }
-        
-        .check:hover {
-          background: #028931;
-          border: none;
-          box-shadow: 0 4px 1px -1px #028931;
-        }
-        
-        p {
-          font-size: 0.8em;
-          line-height: 1.3em;
-        }
-
-        .card {
-          position: absolute;
-          width: 20em;
-          height: 30em;
-          perspective: 500px;
-        }
-        
-        .content {
-          position: absolute;
-          width: 100%;
-          height: 100%;
-          box-shadow: 0 0 15px rgba(0,0,0,0.1);
-        
-          transition: transform 1s;
-          transform-style: preserve-3d;
-        }
-        
-        .flipped .content {
-          transform: rotateY( 180deg ) ;
-          transition: transform 0.5s;
-        }
-
-        /* .card:hover .content {
-          transform: rotateY( 180deg ) ;
-          transition: transform 0.5s;
-        } */
-        
-        .front-card,
-        .back-card {
-          position: absolute;
-          height: 100%;
-          width: 100%;
-          background: white;
-          text-align: center;
-          border-radius: 5px;
-          backface-visibility: hidden;
-        }
-
-        .front-card {
-          background-image: url('/images/front.png');
-          background-size: cover;
-          background-position: center;
-        }
-        
-        .back-card {
-          background: #fff;
-          transform: rotateY( 180deg );
-          padding: .5em;
-        }
-      `}</style>
+      </motion.div>
     </div>
   )
 }
